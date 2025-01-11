d9e6d5d533eb792e7da94f8a1b9652cf1ce3cb16
void <git.email.address@googlemail.com>
2025-01-12 00:15:05 +0100

    Implemented conflict resolution and missing functions

--- game.py
+++ game.py
@@ -19,6 +19,7 @@ class ThumperGame:
 		self.game_ended = False
 
 	def construct_palace(self):
+		self._check_game_ended()
 		if self.current_player.palace:
 			raise ThumperError("Player has already constructed their palace")
 		self._perform_action(ActionType.ECONOMIC, Action.CONSTRUCT_PALACE, spice=6)
@@ -26,33 +27,39 @@ class ThumperGame:
 		self._next_turn()
 
 	def harvester(self):
+		self._check_game_ended()
 		self._perform_action(ActionType.ECONOMIC, Action.HARVESTER)
 		self.current_player.spice += 3
 		self._next_turn()
 
 	def refinery(self):
+		self._check_game_ended()
 		self._perform_action(ActionType.ECONOMIC, Action.REFINERY)
 		self.current_player.spice += 2
 		self.current_player.solari += 1
 		self._next_turn()
 
 	def spice_silo(self):
+		self._check_game_ended()
 		self._perform_action(ActionType.ECONOMIC, Action.SPICE_SILO)
 		self.current_player.spice += self.spice_silo
 		self._next_turn()
 
 	def sell_melange(self):
+		self._check_game_ended()
 		self._perform_action(ActionType.ECONOMIC, Action.SELL_MELANGE, spice=3)
 		self.current_player.solari += 8
 		self._next_turn()
 
 	def secure_contract(self):
+		self._check_game_ended()
 		self._perform_action(ActionType.ECONOMIC, Action.SECURE_CONTRACT)
 		self.current_player.solari += 3
 		self._next_turn()
 
 	# target is the ID of the target player (1 - 4)
 	def stone_burner(self, target):
+		self._check_game_ended()
 		target_index = target - 1
 		if target < 1 or target > PLAYER_COUNT or target_index == self.current_player_index:
 			raise ThumperError("Invalid target player index")
@@ -65,6 +72,7 @@ class ThumperGame:
 		self._next_turn()
 
 	def hire_mercenaries(self, troops_deployed):
+		self._check_game_ended()
 		troops_produced = 2
 		deployment_limit = 3
 		self._check_troops(troops_produced, troops_deployed, deployment_limit)
@@ -73,6 +81,7 @@ class ThumperGame:
 		self._next_turn()
 
 	def quick_strike(self, troops_deployed):
+		self._check_game_ended()
 		troops_produced = 1
 		deployment_limit = 2
 		self._check_troops(troops_produced, troops_deployed, deployment_limit)
@@ -81,11 +90,13 @@ class ThumperGame:
 		self._next_turn()
 
 	def recruitment_center(self):
+		self._check_game_ended()
 		self._perform_action(ActionType.MILITARY, Action.RECRUITMENT_CENTER)
 		self.current_player.troops_garrison += 1
 		self._next_turn()
 
 	def troop_transports(self, troops_deployed):
+		self._check_game_ended()
 		troops_produced = 0
 		deployment_limit = 4
 		self._check_garrison()
@@ -95,11 +106,54 @@ class ThumperGame:
 		self._next_turn()
 
 	def recruit_agent(self):
+		self._check_game_ended()
 		if self.current_player.third_agent:
 			raise ThumperError("Player already recruited their third agent")
 		self._perform_action(ActionType.POLITICAL, Action.RECRUIT_AGENT)
 		self.current_player.third_agent = True
 		self.current_player.agents_left += 1
+		self._next_turn()
+
+	def sardaukar(self):
+		self._check_game_ended()
+		self._perform_action(ActionType.POLITICAL, Action.SARDAUKAR, spice=4)
+		self.current_player.influence += 1
+		self.current_player.troops_garrison += 3
+		self._next_turn()
+
+	def audience_with_emperor(self):
+		self._check_game_ended()
+		self._perform_action(ActionType.POLITICAL, Action.AUDIENCE_WITH_EMPEROR, spice=3)
+		self.current_player.influence += 2
+		self._next_turn()
+
+	def mobilization(self, troops_deployed):
+		self._check_game_ended()
+		troops_produced = 0
+		deployment_limit = 5
+		self._check_garrison()
+		self._check_troops(troops_produced, troops_deployed, deployment_limit)
+		self._perform_action(ActionType.POLITICAL, Action.MOBILIZATION, solari=2)
+		self._deploy_troops(troops_deployed)
+		self._next_turn()
+
+	def seek_allies(self):
+		self._check_game_ended()
+		self._perform_action(ActionType.POLITICAL, Action.SEEK_ALLIES)
+		self.current_player.influence += 1
+		self._next_turn()
+
+	# action_index is the 1-based index into the player's remaining actions
+	# action_type is the desired action to change it to
+	def political_maneuvering(self, action_index, action_type):
+		self._check_game_ended()
+		actions = self.current_player.actions
+		if action_index < 1 or action_index > len(actions):
+			raise ThumperError("Invalid action index")
+		if type(action_type) is not Action:
+			raise ThumperError("Action type is not an action type enum")
+		self._perform_action(ActionType.POLITICAL, Action.POLITICAL_MANEUVERING)
+		actions[action_index - 1] = action_type
 		self._next_turn()
 
 	def _reset_available_actions(self):
@@ -120,6 +174,10 @@ class ThumperGame:
 		self.available_actions.remove(action)
 		self.current_player.spice -= spice
 		self.current_player.solari -= solari
+
+	def _check_game_ended(self):
+		if self.game_ended:
+			raise ThumperError("Tried to perform an action after the game had already ended")
 
 	def _check_garrison(self):
 		if self.current_player.troops_garrison < 1:
@@ -166,13 +224,40 @@ class ThumperGame:
 				self.spice_silo = 1
 			self._reset_available_actions()
 			for player in self.players:
-				player.roll_actions()
+				player.reset()
 		else:
 			self.game_ended = True
 
 	def _resolve_conflict(self):
-		conflict_reward = self.conflict_rewards[self.round - 1]
-		raise NotImplementedError()
+		conflict_rewards = self.conflict_rewards[self.round - 1]
+		conflict_players = filter(lambda p: p.troops_deployed > 0, self.players)
+		reward_groups = {}
+		for player in conflict_players:
+			key = player.troops_deployed
+			if key in reward_groups:
+				reward_groups[key].append(player)
+			else:
+				reward_groups[key] = [player]
+		reward_group_keys = sorted(reward_groups.keys(), reverse=True)
+		for key in reward_group_keys:
+			if len(conflict_rewards) == 0:
+				# There are no conflict rewards left, abort
+				break
+			reward_group = reward_groups[key]
+			if len(reward_group) == 1:
+				# A single player won the reward
+				winner = reward_group[0]
+				reward = conflict_rewards[0]
+				winner.apply_reward(reward)
+				conflict_rewards = conflict_rewards[1:]
+			else:
+				# Multiple players tied for the reward, so their reward level drops by one
+				conflict_rewards = conflict_rewards[1:]
+				if len(conflict_rewards) > 0:
+					# Distribute the rewards among all players who tied
+					reward = conflict_rewards[0]
+					for player in reward_group:
+						player.apply_reward(reward)
 
 	def _set_conflict_rewards(self):
 		rewards = []
--- player.py
+++ player.py
@@ -12,13 +12,21 @@ class ThumperPlayer:
 		self.third_agent = False
 		self.palace = False
 		self.agents_left = INITIAL_AGENTS
-		self.victory_points = None
-		self.roll_actions()
+		self.victory_points = 0
+		self.reset()
 
-	def roll_actions(self):
+	def reset(self):
 		actions = [
 			ActionType.ECONOMIC,
 			ActionType.MILITARY,
 			ActionType.POLITICAL
 		]
+		self.agents_left = 3 if self.third_agent else 2
+		self.troops_deployed = 0
 		self.actions = random.choices(actions, k=4)
+
+	def apply_reward(self, reward):
+		self.victory_points += reward.victory_points
+		self.influence += reward.influence
+		self.spice = reward.spice
+		self.solari = reward.solari
